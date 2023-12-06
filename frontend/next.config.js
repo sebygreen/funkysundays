09cb638d312a998c7fd43ef53887ee8353b5c0fb
Sebastien Green <sebygreen@gmail.com>
2023-12-06 12:18:08 +0100

    fixed fetch urls

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,13 +5,7 @@ module.exports = {
         remotePatterns: [
             {
                 protocol: "http",
-                hostname: "localhost",
-                port: "8090",
-                pathname: "/api/files/**",
-            },
-            {
-                protocol: "http",
-                hostname: "backend",
+                hostname: "127.0.0.1",
                 port: "8090",
                 pathname: "/api/files/**",
             },
