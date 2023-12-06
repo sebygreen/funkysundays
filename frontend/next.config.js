697d53bbf2e5514e77634239e239453c186bf7c7
Sebastien Green <sebygreen@gmail.com>
2023-12-06 13:25:01 +0100

    removed docker integration

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    output: "standalone",
     images: {
         remotePatterns: [
             {
                 protocol: "http",
-                hostname: "backend",
+                hostname: "127.0.0.1",
                 port: "8090",
                 pathname: "/api/files/**",
             },
