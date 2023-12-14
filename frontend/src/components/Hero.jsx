a11000ad5595fe2139f4770d82fc37bc898d6702
Sebastien Green <sebygreen@gmail.com>
2023-12-14 23:30:55 +0100

    added texture to server

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,14 +2,15 @@ import styles from "@/style/Hero.module.css";
 import Image from "next/image";
 import logo from "@/images/logo.png";
 import Upcoming from "./Upcoming";
+import texture from "@/images/grid.png"
 
-export default function Hero({ event }) {
+export default function Hero({event}) {
     return (
         <div
             id="hero"
             className={styles.container}
         >
-            <div className={styles.texture} />
+            <div className={styles.texture} style={{backgroundImage: `url(${texture.src})`}}/>
             <div className="wrapper">
                 <section className={styles.content}>
                     <Image
@@ -17,7 +18,7 @@ export default function Hero({ event }) {
                         alt="Purple logo."
                     />
                     <h1>Making sundays a better day since 2019.</h1>
-                    {event ? <Upcoming event={event} /> : <p>No upcoming events for now.</p>}
+                    {event ? <Upcoming event={event}/> : <p>No upcoming events for now.</p>}
                 </section>
             </div>
         </div>
