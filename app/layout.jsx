3d7696a715ae2086740e55ebba71004724575441
Sebastien Green <45818412+sebygreen@users.noreply.github.com>
2024-06-05 19:17:44 +0200

    enabled umami script

--- app/layout.jsx
+++ app/layout.jsx
@@ -32,7 +32,7 @@ export default function Layout({ children }) {
             />
             <Script
                 async
-                //src="https://umami.smkg.me/script.js"
+                src={"https://umami.smkg.me/script.js"}
                 data-website-id="f29a55dc-c3c6-4ad6-a9ff-4d8d97679cb6"
             />
         </html>
