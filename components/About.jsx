c242b56ae5b1daea80f1031944f490074868393d
Sebastien Green <45818412+sebygreen@users.noreply.github.com>
2024-07-02 20:36:14 +0200

    updated member count

--- components/About.jsx
+++ components/About.jsx
@@ -9,7 +9,7 @@ export default function About() {
             <Stamp />
             <h2>Qui sommes-nous?</h2>
             <p>
-                La famille Funky Sundays, basée à Genève, compte aujourd’hui 12 membres bénévoles et nous sommes motivés
+                La famille Funky Sundays, basée à Genève, compte aujourd’hui 13 membres bénévoles et nous sommes motivés
                 à créer ensemble le meilleur rendez-vous dominical de l’année pour toutes et tous.
             </p>
             <Button type="route" href="/about" icon={<Info size={22} />} text="Découvrir" />
